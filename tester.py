44cea9c73fee5755c117fb49cbde995bd7a0f9fb
noegorod <noelia.gorod@gmail.com>
2019-07-19 16:54:28 -0300

    tratando de aprender a crear objetos y ponerlos en las listas

---
+++ tester.py
@@ -0,0 +1,42 @@
+"""Clase de fila base"""
+class Fila(object):
+    def __init__(self):
+        self.enfila=0
+        self.fila=[]
+
+"""Clase de fila para Clientes Generales"""
+class FilaGeneral(Fila):
+    def insertar(self, cliente):
+        """Inserta un nuevo cliente en la fila"""
+        fila.append(tipo)
+        print(fila)
+        pass
+
+    def atender(self):
+        """Atiende al proximo cliente General"""
+        self.enfila-=1
+        self.fila.pop(0)
+        pass
+
+"""Clase Cliente"""
+class Cliente(object):
+    def __init__(self, numero, cat):
+        self.dni=numero
+        self.categoria=cat
+
+    def modificarcategoria(self, categoria):
+        """modifica el atributo categoria del cliente"""
+        pass
+
+if __name__ == "__main__":
+    """Simulador de burocracia"""
+    thefila = FilaGeneral()
+    tipo = Cliente(420,"General")
+    print("cliente DNI {} entro al coban".format(tipo.dni))
+
+    insertar()
+
+    
+
+
+    
